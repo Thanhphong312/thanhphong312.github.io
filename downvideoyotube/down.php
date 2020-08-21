2a00f0e7493af340bf4353f09400c8f69f11fe1a
thanhphong123 <57260920+Thanhphong312@users.noreply.github.com>
2020-08-21 09:46:21 +0700

    Add files via upload

---
+++ downvideoyotube/down.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>codeDownfile</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h2>DOWNLOAD FILE</h2>
+	<input class="namefile" type="text" placeholder="Select file">
+</body>
+</html>
+<?php 
+	
+?>
+<!--
+	Name:downfile
+	Copyright: thanhphong312
+	Author: thanhphong312
+	Date: 07/07/20 14:20
+	Description: html+php
+-->
